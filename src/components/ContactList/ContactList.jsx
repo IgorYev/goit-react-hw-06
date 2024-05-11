c3d06e60771c1da8c3c646bd4739c9447a83513a
IgorYev <123358377+IgorYev@users.noreply.github.com>
2024-05-11 22:24:17 +0300

    update dz

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,16 +2,10 @@ import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { nanoid } from "nanoid";
 import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
 
 export default function ContactsList() {
-  const value = useSelector((state) => state.contacts.contacts.items);
+  const contacts = useSelector((state) => state.contacts.contacts.items);
   const nameToSearch = useSelector((state) => state.filters.filters.name);
-  const [contacts, setContacts] = useState(value);
-
-  useEffect(() => {
-    setContacts(value);
-  }, [value]);
 
   const filteredContacts = contacts.filter((contact) => {
     return contact.newContact.name
